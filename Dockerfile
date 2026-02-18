89e8b50312c6758f57e5e6cbf2a8cb20e1ed3715
Okafor Obinna <kingsloob1@gmail.com>
2026-02-18 17:57:45 +0100

    fix issue with docker file

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,24 @@
-FROM oven/bun:1.3.9-alpine AS build-container
+FROM node:24.2.0-alpine3.22 AS build-container
 
 # set working directory
-WORKDIR /home/bun/app
+WORKDIR /app
 
 # Set user to root    
 USER root
 
+# Update packages
+RUN apk add --update --no-cache tar && \
+    npm install --force --global npm bun;
+
 # Copy all files and set owner to node
 COPY --chown=node:node . .
 
-# Package artifact and compress
-# RUN (chmod +x ./deployment-config/start.sh) && \
-#     (tar --create --gzip --verbose --file built.tar.gz --exclude="./node_modules" --exclude="./dist" --exclude="./.git" --exclude="./cache" --warning=no-file-changed "../app" || true)
-RUN (chmod +x ./deployment-config/start.sh) && \
-    (tar -czvf built.tar.gz ../app)
+# Install dependencies and build
+RUN bun install --ignore-scripts && \
+    bunx prisma generate && \
+    npm run build && \
+    (chmod +x ./deployment-config/start.sh) && \
+    (tar -czvf built.tar.gz dist prisma supabase templates package.json bun.lock .env)
 
 FROM oven/bun:1.3.9-alpine AS production-container
 
@@ -23,9 +28,12 @@ WORKDIR /home/bun/app
 # Set user to root    
 USER root
 
+RUN apk update --no-cache && apk upgrade --no-cache && \ 
+    apk add --update --no-cache curl tar git rsync bash
+
 # Copy built archive and startup script
-COPY --from=build-container /home/bun/app/built.tar.gz ./
-COPY --from=build-container /home/bun/app/deployment-config/start.sh ./start.sh
+COPY --from=build-container /app/built.tar.gz ./
+COPY --from=build-container /app/deployment-config/start.sh ./start.sh
 
 # Expose port 80
 EXPOSE 80
--- deployment-config/start.sh
+++ deployment-config/start.sh
@@ -5,13 +5,13 @@ INSTALLED_FILE_PATH="/home/bun/installed"
 
 if [ ! -f "$INSTALLED_FILE_PATH" ]; then
     echo "Extract built archive"
-    tar -xvzf built.tar.gz -C /home/bun/ > /dev/null 2>&1
+    tar -xvzf built.tar.gz -C /home/bun/app > /dev/null 2>&1
 
-    echo "Update alpine packages"
+    # echo "Update alpine packages"
     # grep -qxF "http://dl-cdn.alpinelinux.org/alpine/edge/testing" /etc/apk/repositories || echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories; # add edge testing repositories only if it doesnt exists
     # grep -qxF "http://dl-cdn.alpinelinux.org/alpine/edge/community" /etc/apk/repositories || echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories; # add edge community repositories only if it doesnt exists
     # grep -qxF "http://dl-cdn.alpinelinux.org/alpine/edge/main" /etc/apk/repositories || echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories; # add edge main repositories only if it doesnt exists
-    apk update --no-cache && apk upgrade --no-cache;
+    # apk update --no-cache && apk upgrade --no-cache;
 
     # echo "Install curl, g++ and chromium"
     # apk add curl g++ chromium --no-cache;
@@ -19,27 +19,27 @@ if [ ! -f "$INSTALLED_FILE_PATH" ]; then
     # echo "Add Link to ld linux for compatibility with alpine and for uwebsocket.js to work"
     # ln -s /lib/libc.musl-x86_64.so.1 /lib/ld-linux-x86-64.so.2 || true;
 
-    echo "Install curl, git, rsync and bash"
-    apk add --no-cache --update curl git rsync bash;
+    # echo "Install curl, git, rsync and bash"
+    # apk add --no-cache --update curl git rsync bash;
 
     echo "Install dependencies"
     bun install --frozen-lockfile
 
-    echo "Running db migration"
-    if ! bun run --bun --env-file=.env run:db:migration; then
-        echo "Database migration failed. Exiting."
-        exit 1
-    fi
+    # echo "Running db migration"
+    # if ! bunx prisma migrate deploy; then
+    #     echo "Database migration failed. Exiting."
+    #     exit 1
+    # fi
 
     touch "$INSTALLED_FILE_PATH";
 fi
 
 echo "Starting Api Service"
-bun run --bun --env-file=.env ./src/main.ts
+bun run --bun --env-file=.env ./dist/main.js
 
 # git clone git@github.com:CUEngineering/CUCE_BE.git ~/production/repos/CUCE_BE && cd ~/production/repos/CUCE_BE
 # mkdir -p ~/production/docker-compose/services/cuce
 # wsh edit ~/production/docker-compose/services/cuce/cuce.yaml
 # wsh edit ~/production/docker-compose/services/cuce/cuce-backend.env
-# cp ~/production/docker-compose/services/cuce/cuce-backend.env ~/production/repos/CUCE_BE/.env && docker build --no-cache --tag=cuce-backend:production --file ~/production/repos/CUCE_BE/Dockerfile /root/production/repos/CUCE_BE (Always do this after any repo update of the services)
+# cp ~/production/docker-compose/services/cuce/cuce-backend.env ~/production/repos/CUCE_BE/.env && docker build --no-cache --tag=cuce-backend:production --file ~/production/repos/CUCE_BE/Dockerfile --progress=plain /root/production/repos/CUCE_BE (Always do this after any repo update of the services)
 # docker compose --project-name cuce -f ~/production/docker-compose/services/cuce/cuce.yaml up -d
